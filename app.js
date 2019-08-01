a35063e9098891737f6c80ed6421c62634408f6b
Sian Culligan <SianCulligan@gmail.com>
2019-08-01 09:48:19 -0700

    testing JS

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-var getName = prompt('What is your name? I sure hope its Brian.');
+var getName = prompt('What is your name? I sure hope it\'s Brian!');
 var greeting;
 
-if (getName = Brian) {
+if (getName === 'Brian' || getName === 'brian') {
     greeting = 'What up Cozy Pants?!';
-} else if (getName = Sian) {
-    greeting = 'Hi Sian, thanks for writing this JS';
-} else if (getName = " ") {
-    greeting = 'No name? ok, thats cool';
+} else if (getName === 'Sian') {
+    greeting = 'Hi Sian thanks for writing this JS';
+} else if (getName === ' ') {
+    greeting = 'No name, ok, that\'s cool';
 } else {
     greeting = 'Who even are you?'
 }
 
 alert(greeting + ' I sure hope this code works');
 
-// this will update the HTML
 document.write('<h2>' + greeting + '</h2>');
