441332acf006716bfa9d4b01e7a7170627242ec2
Sian Culligan <SianCulligan@gmail.com>
2019-07-31 20:43:40 -0700

    Added JS & background color

---
+++ app.js
@@ -0,0 +1,15 @@
+var nameNow = prompt('What is your first name (please be Brian)?');
+var greeting;
+
+
+
+if (nameNow = Brian) {
+    greeting = 'What up cozy pants!?';
+} else {
+    greeting = 'Hello person that is not Brian'
+}
+
+alert(greeting + ' brian');
+
+// this will update the HTML
+document.write('<h2>' + greeting + '</h2>');
