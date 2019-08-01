fc49e9835fee9c0855c84cbb8c305c751e33a332
Sian Culligan <SianCulligan@gmail.com>
2019-07-31 21:02:44 -0700

    changed alerts

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var hourNow = prompt('what is the current hour?');
+var nameNow = prompt('What is your name? I sure hope its Brian.');
 var greeting;
 
-if (hourNow > 18) {
-    greeting = 'good evening';
-} else if (hourNow > 12) {
-    greeting = 'good afternoon';
-} else if (hourNow > 0) {
-    greeting = 'good morning';
+if (nameNow = Brian) {
+    greeting = 'What up Cozy Pants?!';
+} else if (nameNow = Sian) {
+    greeting = 'Hi Sian, thanks for writing this JS';
+} else if (nameNow = " ") {
+    greeting = 'No name? ok, thats cool';
 } else {
-    greeting = 'welcome to class'
+    greeting = 'Who even are you?'
 }
 
-alert(greeting + ' brian');
+alert(greeting + ' I sure hope this code works');
 
 // this will update the HTML
 document.write('<h2>' + greeting + '</h2>');
